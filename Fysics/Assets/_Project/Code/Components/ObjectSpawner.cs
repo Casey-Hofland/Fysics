2dd5cfa2212a0cb22088fa38a7e5f145a55125f7
Casey Hofland <hofland.casey@gmail.com>
2019-06-06 14:32:05 +0200

    UNet Removed
    Multiplayer wasn't working out, giving me problems and ultimately reducing my motivation for the development of the game. Plus, the UNet is deprecated so there is even less incentive to keep using it. Overall, it feels better to discard it for now.

---
+++ Fysics/Assets/_Project/Code/Components/ObjectSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+	[SerializeField] private List<GameObject> objects = new List<GameObject>();
+	[SerializeField] private Vector2 timeRange = Vector2.zero;
+	[SerializeField] private Transform spawnPoint = null;
+	[SerializeField] private float spawnVelocity = 10f;
+
+	private float timeWait = 0f;
+
+	private void Start()
+	{
+		timeWait = RndTimeWait();
+	}
+
+	private void Update()
+	{
+		timeWait -= Time.deltaTime;
+		if (timeWait <= 0)
+		{
+			Spawn();
+		}
+	}
+
+	void Spawn()
+	{
+		int r = Random.Range(0, objects.Count);
+		GameObject obj = Instantiate(objects[r], spawnPoint.position, spawnPoint.rotation);
+		obj.GetComponent<Rigidbody>().AddForce(spawnVelocity * spawnPoint.up, ForceMode.Impulse);
+		timeWait = RndTimeWait();
+	}
+
+	private float RndTimeWait()
+	{
+		return Random.Range(timeRange.x, timeRange.y);
+	}
+}
