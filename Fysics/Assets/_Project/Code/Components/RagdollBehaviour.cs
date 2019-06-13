a55712cddb82c807408f2e595ab0e2482dda2f99
Casey Hofland <hofland.casey@gmail.com>
2019-06-13 22:11:35 +0200

    Ragdoll Testing

---
+++ Fysics/Assets/_Project/Code/Components/RagdollBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollBehaviour : MonoBehaviour
+{
+	//private Rigidbody controller;
+	private Animator animator;
+	private Collider collider;
+	private Rigidbody[] bones;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+		//controller = GetComponent<Rigidbody>();
+		animator = GetComponent<Animator>();
+		collider = GetComponent<Collider>();
+		bones = transform.GetChild(2).GetComponentsInChildren<Rigidbody>(true);
+		RagdollSetActive(false);
+
+    }
+
+	public void RagdollSetActive(bool value)
+	{
+		animator.enabled = !value;
+		collider.enabled = !value;
+
+		/*
+		foreach (Rigidbody bone in bones)
+		{
+			bone.useGravity = value;
+			bone.isKinematic = !value;
+		}
+
+		if (!value)
+		{
+			Debug.Log("Woesh");
+		}
+		*/
+	}
+
+	private void Update()
+	{
+#if UNITY_EDITOR
+		// Used for Testing
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			RagdollSetActive(true);
+		}
+		if (Input.GetKeyUp(KeyCode.T))
+		{
+			RagdollSetActive(false);
+		}
+#endif
+	}
+}
---
+++ Fysics/Assets/_Project/Code/Components/RagdollPart.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollPart : MonoBehaviour
+{
+	private Rigidbody rigidbody;
+	private Collider collider;
+
+	private RagdollBehaviour rb;
+
+	private void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+		collider = GetComponent<Collider>();
+		rb = GetComponentInParent<RagdollBehaviour>();
+
+		SetActive(false);
+	}
+
+	public void SetActive(bool value)
+	{
+		rigidbody.useGravity = value;
+		rigidbody.isKinematic = !value;
+		collider.enabled = value;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		StartCoroutine(Wait(2f));
+	}
+
+	IEnumerator Wait(float seconds)
+	{
+		rb.RagdollSetActive(true);
+		SetActive(true);
+		yield return new WaitForSeconds(seconds);
+		rb.RagdollSetActive(false);
+		SetActive(false);
+	}
+}
